11a8b9a0cd3391d03eb200f2989ef70e14aae655
Thomas Lamb <thomaslambprogramming@gmail.com>
2023-10-29 21:31:21 +1100

    Added first project setup with window creation

---
+++ GraphicsProject.cpp
@@ -0,0 +1,16 @@
+
+
+#define GLFW_INCLUDE_VULKAN
+#include<GLFW/glfw3.h>
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "HELLO VULKAN", nullptr, nullptr);
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
